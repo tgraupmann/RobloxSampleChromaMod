97794899e8b52d10960eaf9d54492378e549b11a
Tim Graupmann <tgraupmann@gmail.com>
2020-12-18 15:46:03 -0800

    Detect when Roblox logs are written

--- WPF_RobloxChromaMod/MainWindow.xaml.cs
+++ WPF_RobloxChromaMod/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Threading;
 using System.Windows;
 
@@ -30,9 +32,71 @@ namespace WPF_RobloxChromaMod
 
         private void LogWorker()
         {
+            Dictionary<string, long> sizeMap = new Dictionary<string, long>();
+
             while (_mWaitForExit)
             {
+                DirectoryInfo di = new DirectoryInfo(Environment.GetEnvironmentVariable("LocalAppData") + @"\Roblox\logs");
+                if (!di.Exists)
+                {
+                    Thread.Sleep(5000); //wait for folder to exist
+                    continue;
+                }
+                foreach (FileInfo fi in di.GetFiles("*.txt"))
+                {
+                    if (!sizeMap.ContainsKey(fi.FullName))
+                    {
+                        //new log
+                        sizeMap[fi.FullName] = fi.Length;
+                    }
+                    else
+                    {
+                        long oldLength = sizeMap[fi.FullName];
+                        if (fi.Length > oldLength)
+                        {
+                            ReadContent(fi, oldLength);
+                        }
+                        sizeMap[fi.FullName] = fi.Length;
+                    }
+                }
                 Thread.Sleep(100);
+            }
+        }
+
+        void ReadContent(FileInfo fi, long oldLength)
+        {
+            try
+            {
+                using (FileStream fs = File.Open(fi.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        fs.Seek(oldLength, SeekOrigin.Begin);
+                        do
+                        {
+                            string content = sr.ReadLine();
+                            if (content == null)
+                            {
+                                break;
+                            }
+                            const string TOKEN = "ChromaRGB: BtnEffect";
+                            int index = content.IndexOf(TOKEN);
+                            if (index >= 0)
+                            {
+                                string effect = content.Substring(index + TOKEN.Length);
+                                if (true)
+                                {
+
+                                }
+                            }
+                        }
+                        while (true);
+                    }
+                }
+            }
+            catch
+            {
+                Thread.Sleep(3000);
             }
         }
     }
