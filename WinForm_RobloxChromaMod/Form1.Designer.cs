8eb1128148b201bb69a19ddb11ad26fbbe42d266
Tim Graupmann <tgraupmann@gmail.com>
2023-03-21 11:38:34 -0700

    Added WinForm project

---
+++ WinForm_RobloxChromaMod/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WinForm_RobloxChromaMod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this._mPictureBox = new System.Windows.Forms.PictureBox();
+            this._mCaptureTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this._mPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _mPictureBox
+            // 
+            this._mPictureBox.Location = new System.Drawing.Point(67, 113);
+            this._mPictureBox.Name = "_mPictureBox";
+            this._mPictureBox.Size = new System.Drawing.Size(663, 278);
+            this._mPictureBox.TabIndex = 0;
+            this._mPictureBox.TabStop = false;
+            this._mPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureMouseDown);
+            this._mPictureBox.MouseEnter += new System.EventHandler(this.PictureMouseEnter);
+            this._mPictureBox.MouseLeave += new System.EventHandler(this.PictureMouseLeave);
+            this._mPictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureMouseMove);
+            this._mPictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureMouseUp);
+            // 
+            // _mCaptureTimer
+            // 
+            this._mCaptureTimer.Enabled = true;
+            this._mCaptureTimer.Interval = 1000;
+            this._mCaptureTimer.Tick += new System.EventHandler(this._mCaptureTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._mPictureBox);
+            this.Name = "Form1";
+            this.Text = "Roblox Companion App";
+            ((System.ComponentModel.ISupportInitialize)(this._mPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox _mPictureBox;
+        private System.Windows.Forms.Timer _mCaptureTimer;
+    }
+}
+
---
+++ WinForm_RobloxChromaMod/Form1.cs
@@ -0,0 +1,70 @@
+﻿// Ref: https://www.youtube.com/watch?v=6iv7y5v1e0A - C# Capture
+// Ref: https://www.youtube.com/watch?v=Q63GV5tnXN0 - Crop Bitmap
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinForm_RobloxChromaMod
+{
+    public partial class Form1 : Form
+    {
+        Image _mCaptureImage = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void _mCaptureTimer_Tick(object sender, EventArgs e)
+        {
+            _mPictureBox.Hide();
+            SendKeys.Send("{PRTSC}");
+            _mCaptureImage = Clipboard.GetImage();
+            _mPictureBox.Image = _mCaptureImage;
+            _mPictureBox.Show();
+
+            if (_mMouseDown && _mMouseOver)
+            {
+
+            }
+        }
+
+        bool _mMouseDown = false;
+        bool _mMouseOver = false;
+        Point _mMouseMoveStart = Point.Empty;
+        Point _mMouseMoveEnd = Point.Empty;
+
+        private void PictureMouseDown(object sender, MouseEventArgs e)
+        {
+            _mMouseDown = true;
+            _mMouseMoveStart = new Point(e.X, e.Y);
+        }
+
+        private void PictureMouseUp(object sender, MouseEventArgs e)
+        {
+            _mMouseDown = false;
+        }
+
+        private void PictureMouseMove(object sender, MouseEventArgs e)
+        {
+            _mMouseMoveEnd = new Point(e.X, e.Y);
+            if (_mMouseDown && _mMouseOver)
+            {
+                _mPictureBox.Location = 
+                    new Point(_mMouseMoveEnd.X - _mMouseMoveStart.X,
+                    _mMouseMoveEnd.Y - _mMouseMoveStart.Y);
+            }
+        }
+
+        private void PictureMouseEnter(object sender, EventArgs e)
+        {
+            _mMouseOver = true;
+        }
+
+        private void PictureMouseLeave(object sender, EventArgs e)
+        {
+            _mMouseOver = false;
+        }
+    }
+}
