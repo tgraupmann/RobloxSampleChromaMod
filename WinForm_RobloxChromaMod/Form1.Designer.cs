2ac9e2b35e6fd6b40a1ca7e9dfe8c7a3b7aba938
Tim Graupmann <tgraupmann@gmail.com>
2023-03-21 14:46:05 -0700

    Added color sampling

--- WinForm_RobloxChromaMod/Form1.Designer.cs
+++ WinForm_RobloxChromaMod/Form1.Designer.cs
@@ -31,15 +31,22 @@
             this.components = new System.ComponentModel.Container();
             this._mPictureBox = new System.Windows.Forms.PictureBox();
             this._mCaptureTimer = new System.Windows.Forms.Timer(this.components);
+            this._mDebugLabel1 = new System.Windows.Forms.Label();
+            this._mDebugLabel2 = new System.Windows.Forms.Label();
+            this._mDebugLabel3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this._mButtonColor = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this._mLabelButtonEffect = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this._mPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // _mPictureBox
             // 
             this._mPictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this._mPictureBox.Location = new System.Drawing.Point(67, 113);
+            this._mPictureBox.Location = new System.Drawing.Point(12, 12);
             this._mPictureBox.Name = "_mPictureBox";
-            this._mPictureBox.Size = new System.Drawing.Size(663, 278);
+            this._mPictureBox.Size = new System.Drawing.Size(400, 400);
             this._mPictureBox.TabIndex = 0;
             this._mPictureBox.TabStop = false;
             this._mPictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureMouseDown);
@@ -51,19 +58,90 @@
             // _mCaptureTimer
             // 
             this._mCaptureTimer.Enabled = true;
-            this._mCaptureTimer.Interval = 1000;
             this._mCaptureTimer.Tick += new System.EventHandler(this._mCaptureTimer_Tick);
+            // 
+            // _mDebugLabel1
+            // 
+            this._mDebugLabel1.AutoSize = true;
+            this._mDebugLabel1.Location = new System.Drawing.Point(442, 12);
+            this._mDebugLabel1.Name = "_mDebugLabel1";
+            this._mDebugLabel1.Size = new System.Drawing.Size(58, 17);
+            this._mDebugLabel1.TabIndex = 1;
+            this._mDebugLabel1.Text = "Position";
+            // 
+            // _mDebugLabel2
+            // 
+            this._mDebugLabel2.AutoSize = true;
+            this._mDebugLabel2.Location = new System.Drawing.Point(442, 62);
+            this._mDebugLabel2.Name = "_mDebugLabel2";
+            this._mDebugLabel2.Size = new System.Drawing.Size(38, 17);
+            this._mDebugLabel2.TabIndex = 2;
+            this._mDebugLabel2.Text = "Start";
+            // 
+            // _mDebugLabel3
+            // 
+            this._mDebugLabel3.AutoSize = true;
+            this._mDebugLabel3.Location = new System.Drawing.Point(442, 110);
+            this._mDebugLabel3.Name = "_mDebugLabel3";
+            this._mDebugLabel3.Size = new System.Drawing.Size(33, 17);
+            this._mDebugLabel3.TabIndex = 3;
+            this._mDebugLabel3.Text = "End";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(442, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Button Effect Color";
+            // 
+            // _mButtonColor
+            // 
+            this._mButtonColor.AutoSize = true;
+            this._mButtonColor.Location = new System.Drawing.Point(442, 187);
+            this._mButtonColor.Name = "_mButtonColor";
+            this._mButtonColor.Size = new System.Drawing.Size(41, 17);
+            this._mButtonColor.TabIndex = 5;
+            this._mButtonColor.Text = "Color";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(442, 234);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Current Button Effect";
+            // 
+            // _mLabelButtonEffect
+            // 
+            this._mLabelButtonEffect.AutoSize = true;
+            this._mLabelButtonEffect.Location = new System.Drawing.Point(442, 260);
+            this._mLabelButtonEffect.Name = "_mLabelButtonEffect";
+            this._mLabelButtonEffect.Size = new System.Drawing.Size(66, 17);
+            this._mLabelButtonEffect.TabIndex = 7;
+            this._mLabelButtonEffect.Text = "No Effect";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(692, 421);
+            this.Controls.Add(this._mLabelButtonEffect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this._mButtonColor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._mDebugLabel3);
+            this.Controls.Add(this._mDebugLabel2);
+            this.Controls.Add(this._mDebugLabel1);
             this.Controls.Add(this._mPictureBox);
             this.Name = "Form1";
             this.Text = "Roblox Companion App";
+            this.TopMost = true;
             ((System.ComponentModel.ISupportInitialize)(this._mPictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,6 +149,13 @@
 
         private System.Windows.Forms.PictureBox _mPictureBox;
         private System.Windows.Forms.Timer _mCaptureTimer;
+        private System.Windows.Forms.Label _mDebugLabel1;
+        private System.Windows.Forms.Label _mDebugLabel2;
+        private System.Windows.Forms.Label _mDebugLabel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label _mButtonColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label _mLabelButtonEffect;
     }
 }
 
--- WinForm_RobloxChromaMod/Form1.cs
+++ WinForm_RobloxChromaMod/Form1.cs
@@ -18,39 +18,146 @@ namespace WinForm_RobloxChromaMod
 
         private void _mCaptureTimer_Tick(object sender, EventArgs e)
         {
+            // replace clipboard operation later
             SendKeys.Send("{PRTSC}");
-            _mCaptureImage = Clipboard.GetImage();
 
-            // do some cropping
+            try
+            {
+                // clipboard operation can fail
+                _mCaptureImage = Clipboard.GetImage();
 
-            _mPictureBox.Image = _mCaptureImage;
-            _mPictureBox.Invalidate();
+                // do some cropping
+                Graphics g = _mPictureBox.CreateGraphics();
+                Rectangle rectCropArea = new Rectangle(
+                    _mMouseMoveStart.X - _mMouseMoveEnd.X,
+                    _mMouseMoveStart.Y - _mMouseMoveEnd.Y,
+                    _mPictureBox.Width,
+                    _mPictureBox.Height);
+                g.DrawImage(_mCaptureImage, new Rectangle(0, 0, _mPictureBox.Width, _mPictureBox.Height), rectCropArea, GraphicsUnit.Pixel);
+
+                // read the center pixel
+                Bitmap bmp = new Bitmap(_mCaptureImage);
+                Color color = bmp.GetPixel(rectCropArea.X + _mPictureBox.Width / 2,
+                    rectCropArea.Y + _mPictureBox.Height / 2);
+                _mButtonColor.Text = string.Format("{0},{1},{2}", color.R, color.G, color.B);
+
+                // cleanup
+                bmp.Dispose();
+                _mCaptureImage.Dispose();
+                g.Dispose();
+
+                if (MatchColor(color, 255, 0, 0))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect1";
+                }
+                else if (MatchColor(color, 0, 255, 0))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect2";
+                }
+                else if (MatchColor(color, 0, 0, 255))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect3";
+                }
+                else if (MatchColor(color, 191, 0, 0))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect4";
+                }
+                else if (MatchColor(color, 0, 191, 0))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect5";
+                }
+                else if (MatchColor(color, 0, 0, 191))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect6";
+                }
+                else if (MatchColor(color, 127, 0, 0))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect7";
+                }
+                else if (MatchColor(color, 0, 127, 0))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect8";
+                }
+                else if (MatchColor(color, 0, 0, 127))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect9";
+                }
+                else if (MatchColor(color, 63, 0, 0))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect10";
+                }
+                else if (MatchColor(color, 0, 63, 0))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect11";
+                }
+                else if (MatchColor(color, 0, 0, 63))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect12";
+                }
+                else if (MatchColor(color, 255, 255, 0))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect13";
+                }
+                else if (MatchColor(color, 0, 255, 255))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect14";
+                }
+                else if (MatchColor(color, 255, 0, 255))
+                {
+                    _mLabelButtonEffect.Text = "BtnEffect15";
+                }
+                else
+                {
+                    _mLabelButtonEffect.Text = "None";
+                }
+            }
+            catch
+            {
+                _mMouseMoveStart = Point.Empty;
+                _mMouseMoveEnd = Point.Empty;
+                _mMouseMoveOffset = Point.Empty;
+            }
+        }
+
+        private bool MatchColor(Color color, byte red, byte green, byte blue)
+        {
+            return (color.R == red && color.G == green && color.B == blue);
         }
 
         bool _mMouseDown = false;
         bool _mMouseOver = false;
         Point _mMouseMoveStart = Point.Empty;
         Point _mMouseMoveEnd = Point.Empty;
+        Point _mMouseMoveOffset = Point.Empty;
 
         private void PictureMouseDown(object sender, MouseEventArgs e)
         {
-            _mMouseDown = true;
-            _mMouseMoveStart = new Point(e.X, e.Y);
+            if (!_mMouseDown && _mMouseOver)
+            {
+                _mMouseDown = true;
+                _mMouseMoveStart = new Point(e.X + _mMouseMoveOffset.X, e.Y + _mMouseMoveOffset.Y);
+                _mMouseMoveOffset = Point.Empty;
+                _mDebugLabel1.Text = string.Format("Start {0},{1}", _mMouseMoveStart.X, _mMouseMoveStart.Y);
+            }
         }
 
         private void PictureMouseUp(object sender, MouseEventArgs e)
         {
+            if (_mMouseDown)
+            {
+                _mMouseMoveEnd = new Point(e.X + _mMouseMoveOffset.X, e.Y + _mMouseMoveOffset.Y);
+                _mMouseMoveOffset = new Point(_mMouseMoveStart.X - _mMouseMoveEnd.X, _mMouseMoveStart.Y - _mMouseMoveEnd.Y);
+                _mDebugLabel3.Text = string.Format("Offset {0},{1}", _mMouseMoveOffset.X, _mMouseMoveOffset.Y);
+            }
             _mMouseDown = false;
         }
 
         private void PictureMouseMove(object sender, MouseEventArgs e)
         {
-            _mMouseMoveEnd = new Point(e.X, e.Y);
-            if (_mMouseDown && _mMouseOver)
+            if (_mMouseDown)
             {
-                _mPictureBox.Location = 
-                    new Point(_mMouseMoveEnd.X - _mMouseMoveStart.X,
-                    _mMouseMoveEnd.Y - _mMouseMoveStart.Y);
+                _mMouseMoveEnd = new Point(e.X + _mMouseMoveOffset.X, e.Y + _mMouseMoveOffset.Y);
+                _mDebugLabel2.Text = string.Format("End {0},{1}", _mMouseMoveEnd.X, _mMouseMoveEnd.Y);
             }
         }
 
